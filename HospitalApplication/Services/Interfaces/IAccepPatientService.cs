a8aec9be257f981bf3b9b6333dbcabdc9706d0cc
Kirill <kvest1997@gmail.com>
2024-12-22 00:06:18 +0700

    Создан интерфеййс сервиса для принятия записей

---
+++ HospitalApplication/Services/Interfaces/IAccepPatientService.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.DAL.Entityes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApplication.Services.Interfaces
+{
+    interface IAccepPatientService
+    {
+        Task<Appointment> GetAppointments();
+    }
+}
