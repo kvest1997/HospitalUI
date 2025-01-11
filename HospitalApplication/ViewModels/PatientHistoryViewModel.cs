e4a84c54eb15761cab4d58a984770c309d0a6e60
Kirill <kvest1997@gmail.com>
2025-01-12 01:36:55 +0700

    Доработал просмотр истории пациента

--- HospitalApplication/ViewModels/PatientHistoryViewModel.cs
+++ HospitalApplication/ViewModels/PatientHistoryViewModel.cs
@@ -2,6 +2,7 @@
 using HospitalUI.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -10,14 +11,67 @@ namespace HospitalApplication.ViewModels
     internal class PatientHistoryViewModel : ViewModel
     {
         private IEnumerable<Appointment> _appointments;
+        private ObservableCollection<PrescribedTreatment> _prescribedTreatments;
+        private ObservableCollection<ExaminationResult> _examinationResults;
+
         private Doctor _doctor;
         private Patient _patient;
+
+        private Appointment _selectedAppointment;
+        private ExaminationResult _selectedExaminationResult;
+        private PrescribedTreatment _selectedPrescribedTreatment;
+
         public PatientHistoryViewModel(IEnumerable<Appointment> appointments)
         {
             _appointments = appointments;
 
             _doctor = appointments.FirstOrDefault(x => x.Doctor != null)?.Doctor;
             _patient = appointments.FirstOrDefault(x => x.Patient != null)?.Patient;
+        }
+
+        public ExaminationResult SelectedExaminationResults
+        {
+            get => _selectedExaminationResult;
+            set
+            {
+                Set(ref _selectedExaminationResult, value);
+
+                if(_selectedExaminationResult != null)
+                    PrescribedTreatments = new ObservableCollection<PrescribedTreatment>(_selectedExaminationResult.PrescribedTreatments);
+            }
+        }
+
+        public PrescribedTreatment SelectedPrescribedTreatment
+        {
+            get => _selectedPrescribedTreatment;
+            set => Set(ref _selectedPrescribedTreatment, value);
+        }
+
+        public Appointment SelectedAppointment
+        {
+            get => _selectedAppointment;
+            set
+            {
+                Set(ref _selectedAppointment, value);
+
+                SelectedExaminationResults = null;
+                SelectedPrescribedTreatment = null;
+                PrescribedTreatments = null;
+
+                ExaminationResults = new ObservableCollection<ExaminationResult>(_selectedAppointment.ExaminationResults);
+            }
+        }
+
+        public ObservableCollection<PrescribedTreatment> PrescribedTreatments
+        {
+            get => _prescribedTreatments;
+            set => Set(ref _prescribedTreatments, value);
+        }
+
+        public ObservableCollection<ExaminationResult> ExaminationResults
+        {
+            get => _examinationResults;
+            set => Set(ref _examinationResults, value);
         }
 
         public Doctor Doctor
